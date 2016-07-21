90989b553b8d9b609737b7e7e4ed643cc573c5e6
Dan Cusher <dscush@gmail.com>
2016-07-21 21:07:29 +0000

    Increased feedback from controller to view

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -5,23 +5,42 @@
 var boardCtlr = {};
 var board = new BoardModel();
 
-boardCtlr.drawBoard = function(board) {
+boardCtlr.drawBoard = function(board, msg, lastMove) {
     for (var i in board) {
         if (board[i] == 0) {
             $('#' + i).text('');
         } else {
             $('#' + i).text(board[i]);
         }
+        $('#' + i).removeClass("last-move");
     }
+    $('#' + lastMove).addClass("last-move");
+    $('#turn').text(msg)
 };
+
 boardCtlr.playMove = function(id) {
-    board.playMove(id);
-    boardCtlr.drawBoard(board.board);
-}
+    var playerTurn = board.playMove(id);
+    var msg;
+    if (playerTurn === -1) {
+        msg = "Game over - "
+        if (board.store[0] > board.store[1]) {
+            msg += "bottom player wins!"
+        } else if (board.store[0] < board.store[1]) {
+            msg += "top player wins!"
+        } else {
+            msg += "It's a Tie!"
+        }
+    } else if (playerTurn === 0) {
+        msg = "Bottom player's turn"
+    } else {
+        msg = "Top player's turn"
+    }
+    boardCtlr.drawBoard(board.board, msg, id);
+};
 
 $(document).ready(function(){
     $('button.pit').click(function(){
         boardCtlr.playMove($(this).attr('id'));
     });
-    boardCtlr.drawBoard(board.board);
+    boardCtlr.drawBoard(board.board, "Bottom player's goes first");
 });
